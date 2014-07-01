96af26c485ff34266e3af65a81adeb45da70684b
alxndrkalinin <alxndrkalinin@gmail.com>
2014-07-01 15:11:11 -0400

    chore: change karma settings as described in docs #SOCRFW-108

--- test/client/karma.conf.js
+++ test/client/karma.conf.js
@@ -86,7 +86,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
      plugins: [
       'karma-jasmine',
