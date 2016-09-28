6a98e9c390aec5dc648bf132001ab301d619d8cc
alxndrkalinin <alxndrkalinin@gmail.com>
2016-09-28 19:33:18 -0400

    fix(server.js): fix path to the build

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3000, function(){
-  console.log('Server running on 300...');
+connect().use(serveStatic(__dirname+"/_build")).listen(3000, function(){
+  console.log('Server running on 3000...');
 });
