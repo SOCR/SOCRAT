f0de5b4f6ed4325229f1542966aec7550304dd8e
peijli <peijli@umich.edu>
2021-04-02 16:55:57 +0800

    feat(teachable-machine): full support for loading multidimensional features and labels

--- dev-ml/script.js
+++ dev-ml/script.js
@@ -1,6 +1,13 @@
 // DEVELOPMENT PLANS FOR APRIL 1
 // TODO: Complete Prediction Input
 // TODO: Multiple Inputs
+/*
+  Multiple inputs would undoubtedly have to account for the following:
+  - Difference in data selection and extraction
+  - Difference in model specification
+  - Difference in tensor conversion
+  - Difference in testing and data prediction UI.
+ */
 // TODO: Layers Specifier (Make something that appears to work...)
 
 
@@ -13,14 +20,18 @@ let infoField = document.getElementById('fileInfoContainer')
 // Submit file and load data
 let viewButton = document.getElementById('viewFile')
 
+// Set of fields for selecting and loading feature and label data
+// TODO: Set up multiple feature input
 let featureField = document.getElementById('featureSelection')
 let labelField = document.getElementById('labelSelection')
 let confirmButton = document.getElementById('confirmSelection')
 let dataLoaded = document.getElementById('loadComplete')
 
+// let testMultiple = document.getElementById('multidimensionalTest')
+
 let trainingStarter = document.getElementById('trainingStarter')
 let testingStarter = document.getElementById('testingStarter')
-let predictionInput = document.getElementById('predictionInput')
+let modelPredictionContainer = document.getElementById('modelPredictionContainer')
 let predictionButton = document.getElementById('confirmPredictionInput')
 let predictionOutput = document.getElementById('predictedValueContainer')
 
@@ -32,12 +43,12 @@ let epochSelector = document.getElementById('epochNumber')
 
 let csvObject;
 let file, model, data = [], tensorData;
-// let features = [], labels = [];
 
-let featureName, labelName, predictFeature;
+let featureName, labelName;
+let predictionInputTrackers = [];
+let predictionInputData = [];
 
-let batchSize, epochs;
-
+/** Operation to be completed when the user selects a file upload */
 function uploadOperation() {
 	file = this.files[0];
 	const timeModified = new Date(file.lastModified).toLocaleString('en-US');
@@ -61,15 +72,21 @@ function uploadOperation() {
 	});
 }
 
+
+/**
+ * Loads attributes of the dataset into the selection fields for features and
+ * labels
+ */
 function loadAttributes() {
 	// Extract first row from the csv
 	// I know that not all data are like this!
 	let attributes = csvObject['data'][0]
 	if (featureField)
 	featureField.innerHTML = "";
-
 	labelField.innerHTML = "";
 
+	// TODO: Group data by types, for example, doubles vs. strings vs. other
+  //  pieces of shit.
 	for (let i = 0; i < attributes.length; i++) {
 		featureField.innerHTML += "<option value = \"" + attributes[i] +
 			"\">" + attributes[i] + "</option>";
@@ -79,24 +96,85 @@ function loadAttributes() {
 
 }
 
+/**
+ * Return an array of the selected values select is an HTML select element
+ * @param A variable pointing to an HTML <select> tag
+ */
+function getSelectValues(select) {
+  let result = [];
+  const options = select && select.options;
+  let opt;
+
+  for (let i=0, iLen=options.length; i<iLen; i++) {
+    opt = options[i];
+
+    if (opt.selected) {
+      result.push(opt.value || opt.text);
+    }
+  }
+  return result;
+}
+
+
+/**
+ * Load data into the feature and label fields according to the attributes
+ * selected by the user
+ */
 function loadData() {
+  data = [];
 	dataLoaded.innerText = '';
 
-	const featureIndex = featureField.selectedIndex;
-	const labelIndex = labelField.selectedIndex;
+	// String names of the features and labels
+	const featureList = getSelectValues(featureField);
+	const labelList = getSelectValues(labelField);
+	// Indices of the features and labels in the first row of the dataset
+  const featureIndices = featureList.map(x => csvObject['data'][0].indexOf(x))
+  const labelIndices = labelList.map(x => csvObject['data'][0].indexOf(x))
 
-	let dataItem
-	featureName = csvObject['data'][0][featureIndex]
-	labelName = csvObject['data'][0][labelIndex]
+	console.log("featureList: ", featureList)
+  console.log("labelList: ", labelList)
+  console.log("featureIndices: ", featureIndices)
+  console.log("labelIndices: ", labelIndices)
 
-	// Extract data from the csv object by column
-	// Skip first row with the string attributes
-	for (let i = 1; i < csvObject['data'].length; i++) {
-		dataItem = {}
-		dataItem[featureName] = csvObject['data'][i][featureIndex]
-		dataItem[labelName] = csvObject['data'][i][labelIndex]
-		data.push(dataItem)
-	}
+	// Load prediction parameters
+  modelPredictionContainer.innerHTML = "";
+  predictionInputTrackers = [];
+  for (let i=0; i < featureList.length; i++) {
+    modelPredictionContainer.innerHTML += "<div>"+ featureList[i] + "</div>" +
+      "<input type=\"text\" id=\"" + featureList[i] +
+      "\" name=\"" + featureList[i] + "\"/> ";
+    predictionInputTrackers.push(document.getElementById(featureList[i]))
+  }
+
+  /*
+   * A sample dataItem in the data array is in the following form...
+   * dataItem = {
+   *   "features": [featureOne, featureTwo, etc.];
+   *   "labels": [labelOne, labelTwo, etc.];
+   * }
+   *
+   * In 1D, this would equate to...
+   * dataItem = {
+   *   "features": [feature]
+   *   "labels": [label]
+   * }
+   */
+  // Extract data from the csv object by column
+  // Skip first row with the string attributes
+  for (let i = 1; i < csvObject['data'].length; i++) {
+    let dataItem = {}
+    dataItem['features'] = featureIndices.map(x => csvObject['data'][i][x])
+    dataItem['labels'] = labelIndices.map(x => csvObject['data'][i][x])
+    if (i == 1) console.log(dataItem) // for debugging
+    data.push(dataItem)
+  }
+
+  // Ad-hoc solution for single input and output
+	// featureName = featureList[0]
+	// labelName = labelList[0]
+  // const featureIndex = csvObject['data'][0].indexOf(featureName);
+  // const labelIndex = csvObject['data'][0].indexOf(labelName);
+	// convertArrayToTensor(data, featureList, labelList)
 
 	// const datasetSize = data.length;
 	dataLoaded.innerText += 'Features and Labels loaded and converted to' +
@@ -105,6 +183,37 @@ function loadData() {
 		data.length + '\n';
 }
 
+// TODO: This may not be the most efficient way to loop through a f**kton of
+//  data
+function convertArrayToTensor(data, featureList, labelList) {
+  // Step 0. Clean data
+  for (let inputItem = 0; inputItem < featureList.length; inputItem++) {
+    data = data.filter(item => item[featureList[inputItem]] != null);
+  }
+  for (let labelItem = 0; labelItem < labelList.length; labelItem++) {
+    data = data.filter(item => item[labelList[labelItem]] != null);
+  }
+
+  // Step 1. Shuffle the data
+  tf.util.shuffle(data);
+
+  // Step 2. Map features and labels to separate arrays
+  const inputData = []; const labelData = [];
+  const inputs = []; const labels = [];
+
+  // Step 2.1 Map features into the inputData array
+  for (let i = 0; i < featureList.length; i++) {
+    inputData.push(data.map(d => d[featureList[i]]));
+  }
+  // Step 2.2 Map labels into the labelData array
+  for (let i = 0; i < labelList.length; i++) {
+    labelData.push(data.map(d => d[labelList[i]]));
+  }
+  // Step 3 Convert arrays into multidimensional tensors
+  const inputShape = []
+}
+
+
 /**
  * Convert the input data to tensors that we can use for machine
  * learning. We will also do the important best practices of _shuffling_
@@ -112,23 +221,27 @@ function loadData() {
  * MPG on the y-axis.
  */
 function convertToTensor(data, featureName, labelName) {
+  // function convertToTensor(data, featureList, labelList)
 	// Wrapping these calculations in a tidy will dispose any
 	// intermediate tensors.
 
 	return tf.tidy(() => {
-		// Step 0. Drop the null data
+	  // Step 0. Drop the null data
 		data = data.filter(item =>
-			(item[featureName] != null && item[labelName] != null))
+			(item['features'][0] != null && item['labels'][0] != null))
 
 		// Step 1. Shuffle the data
 		tf.util.shuffle(data);
 
 		// Step 2. Convert data to Tensor
-		const inputs = data.map(d => d[featureName])
-		const labels = data.map(d => d[labelName]);
+		const inputs = data.map(d => d['features'][0])
+		const labels = data.map(d => d['labels'][0]);
 
 		const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
 		const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+		console.log('input shape: ', inputTensor.shape)
+    console.log('label shape: ', labelTensor.shape)
 
 		// Step 3. Normalize the data to the range 0 - 1 using min-max scaling
 		const inputMax = inputTensor.max();
@@ -166,6 +279,11 @@ function createLinearModel () {
 	return model;
 }
 
+
+/**
+ * Train a TensorFlow.JS model according to the specified model, features,
+ * and labels.
+ */
 async function trainModel(model, inputs, labels) {
 
 	const optimizerType = optimizerSelector.value
@@ -280,14 +398,22 @@ async function run() {
 	document.getElementById('trainingOutput').innerText = "Done Training!"
 }
 
+
+// Try to reformat this...
 async function predictValue() {
-	predictFeature = parseFloat(predictionInput.value)
-	console.log(predictFeature)
+  predictionInputData = [];
+  for (let i = 0; i < predictionInputTrackers.length; i++) {
+    // Load input values
+    predictionInputData.push(parseFloat(predictionInputTrackers[i].value))
+  }
+  console.log(predictionInputData)
 }
 
 inputField.addEventListener('change', uploadOperation)
 viewButton.addEventListener('click', loadAttributes)
 confirmButton.addEventListener('click', loadData)
+
+// testMultiple.addEventListener('click', convertArrayToTensor)
 
 trainingStarter.addEventListener('click', run)
 testingStarter.addEventListener('click', testModel)
